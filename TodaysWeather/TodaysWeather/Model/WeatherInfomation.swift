a88828cff25a93567eb3c704c71c670d16f55c29
horeng2 <huski.a.a@gmail.com>
2022-06-09 17:10:16 +0900

    feat: 날씨정보타입 WeatherInfomation 구현

---
+++ TodaysWeather/TodaysWeather/Model/WeatherInfomation.swift
@@ -0,0 +1,19 @@
+//
+//  TodaysWeatherInfo.swift
+//  TodaysWeather
+//
+//  Created by 서녕 on 2022/06/09.
+//
+
+import Foundation
+
+struct WeatherInfomation {
+    let wheatherIcon: String
+    let wheatherCondition: String
+    let cityName: String
+    let currentTemperatures: Double
+    let currentHumidity: Double
+    let sensibleTemperatures: Double
+    let maximumTemperatures: Double
+    let minimumTemperatures: Double
+}
