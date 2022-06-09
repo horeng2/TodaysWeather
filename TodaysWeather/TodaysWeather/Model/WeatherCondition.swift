a749c946f5f8c6fd366c2e15781b2d17c2060faf
horeng2 <huski.a.a@gmail.com>
2022-06-09 18:06:19 +0900

    feat: WeatherCondition 타입 구현

---
+++ TodaysWeather/TodaysWeather/Model/WeatherCondition.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCondition.swift
+//  TodaysWeather
+//
+//  Created by 서녕 on 2022/06/09.
+//
+
+import Foundation
+
+enum WeatherCondition: String {
+    case clear = "맑음"
+    case cloudy = "구름 많음"
+    case overcast = "흐림"
+    case rainy = "비"
+    case snow = "눈"
+}
