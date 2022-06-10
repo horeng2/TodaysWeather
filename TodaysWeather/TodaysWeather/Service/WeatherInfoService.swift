8e5c87fa7c5f38e22892006df28bb87ecac802ae
horeng2 <huski.a.a@gmail.com>
2022-06-11 01:43:28 +0900

    refactor: WeatherInfoService 로직 수정

--- TodaysWeather/TodaysWeather/Service/WeatherInfoService.swift
+++ TodaysWeather/TodaysWeather/Service/WeatherInfoService.swift
@@ -10,34 +10,17 @@ import Foundation
 class WeatherInfoService {
     let weatherInfoRepository = WeatherInfoRepository()
     let geoInfoService = GeoInfoService()
-    
-    var weatherInfoUpdated: () -> Void = {}
 
-    var totalWeatherInfo = [City: WeatherInfo]()
-    {
-        didSet {
-            if totalWeatherInfo.count == City.allCases.count {
-                weatherInfoUpdated()
-            }
+    func fetchWeatherInfo(city: City, completionHandler: @escaping (Result<WeatherInfo, NetworkError>) -> Void) {
+        guard let geoInfo = geoInfoService.totalGeoInfo[city] else {
+            return
         }
-    }
-    
-    init() {
-        self.fetchOfAllCityWeatherInfo()
-    }
-    
-    func fetchOfAllCityWeatherInfo() {
-        City.allCases.forEach { city in
-            guard let geoInfo = self.geoInfoService.totalGeoInfo[city] else {
-                return
-            }
-            self.weatherInfoRepository.fetchWeatherInfo(by: geoInfo) { (result: Result<WeatherInfo, NetworkError>) in
-                switch result {
-                case .success(let weatherInfo):
-                    self.totalWeatherInfo.updateValue(weatherInfo, forKey: city)
-                case .failure(_):
-                    return
-                }
+        self.weatherInfoRepository.fetchWeatherInfo(by: geoInfo) { (result: Result<WeatherInfo, NetworkError>) in
+            switch result {
+            case .success(let weatherInfo):
+                completionHandler(.success(weatherInfo))
+            case .failure(let error):
+                completionHandler(.failure(error))
             }
         }
     }
