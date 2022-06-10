e22c8b33ce81c5c37ae8a263f289737b2df6caef
horeng2 <huski.a.a@gmail.com>
2022-06-11 02:59:24 +0900

    feat: WeatherListViewModel 구현

--- TodaysWeather/TodaysWeather/ViewModel/WeatherListViewModel.swift
+++ TodaysWeather/TodaysWeather/ViewModel/WeatherListViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+class WeatherListViewModel {
+    let weatherInfoService = WeatherInfoService()
+    let geoInfoService = GeoInfoService()
+    let geoInfo: [City: GeoInfo]
+    
+    var weatherInfoUpdated: () -> Void = {}
+    var totalWeatherInfo = [City: WeatherInfo]()
+    {
+        didSet {
+            if self.totalWeatherInfo.count == City.allCases.count {
+                self.weatherInfoUpdated()
+            }
+        }
+    }
+    
+    init() {
+        self.geoInfo = self.geoInfoService.geoInfoOfAllCities()
+    }
+    
+    func fetchWeatherList() {
+        City.allCases.forEach { city in
+            guard let geoInfo = self.geoInfo[city] else {
+                return
+            }
+            self.fetchWeatherInfo(city: city, geoInfo: geoInfo)
+        }
+    }
+    
+    func fetchWeatherInfo(city: City, geoInfo: GeoInfo) {
+        guard let geoInfo = self.geoInfoService.totalGeoInfo[city] else {
+            return
+        }
+        self.weatherInfoService.fetchWeatherInfo(geoInfo: geoInfo) { (result: Result<WeatherInfo, NetworkError>) -> Void in
+            switch result {
+            case .success(let weatherInfo):
+                self.totalWeatherInfo.updateValue(weatherInfo, forKey: city)
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
