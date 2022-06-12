f597cfc30ceecb50766a8828e64f5c5a110fc067
horeng2 <huski.a.a@gmail.com>
2022-06-12 21:14:54 +0900

    refactor: 연관 타입들의 리팩토링에 맞추어 뷰모델 리팩토링

--- TodaysWeather/TodaysWeather/Scene/ListView/ViewModel/WeatherListViewModel.swift
+++ TodaysWeather/TodaysWeather/Scene/ListView/ViewModel/WeatherListViewModel.swift
@@ -10,41 +10,11 @@ import Foundation
 class WeatherListViewModel {
     let weatherInfoService = WeatherInfoService()
     let geoInfoService = GeoInfoService()
-    let geoInfo: [City: GeoInfo]
-    
-    var weatherInfoUpdated: () -> Void = {}
-    var totalWeatherInfo = [City: WeatherInfo]()
-    {
-        didSet {
-            if self.totalWeatherInfo.count == City.allCases.count {
-                self.weatherInfoUpdated()
-            }
-        }
-    }
-    
-    init() {
-        self.geoInfo = self.geoInfoService.geoInfoOfAllCities()
-    }
-    
-    func fetchWeatherList() {
-        City.allCases.forEach { city in
-            guard let geoInfo = self.geoInfo[city] else {
-                return
-            }
-            self.fetchWeatherInfo(city: city, geoInfo: geoInfo)
-        }
-    }
-    
-    func fetchWeatherInfo(city: City, geoInfo: GeoInfo) {
-        guard let geoInfo = self.geoInfoService.totalGeoInfo[city] else {
-            return
-        }
-        self.weatherInfoService.fetchWeatherInfo(geoInfo: geoInfo) { (result: Result<WeatherInfo, NetworkError>) -> Void in
-            switch result {
-            case .success(let weatherInfo):
-                self.totalWeatherInfo.updateValue(weatherInfo, forKey: city)
-            case .failure(_):
-                return
+
+    func fetchWeatherInfo(of city: City, completionHandler: @escaping (WeatherInfo) -> Void) {
+        self.geoInfoService.decodeGeoInfo(of: city) { geoInfo in
+            self.weatherInfoService.decodeWeatherInfo(at: geoInfo) { weatherInfo in
+                completionHandler(weatherInfo)
             }
         }
     }
