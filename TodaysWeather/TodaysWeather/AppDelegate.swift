4368e570fde7222c507f121b7fdbc640ae081f78
horeng2 <huski.a.a@gmail.com>
2022-06-14 18:05:18 +0900

    refactor: 두번째 뷰 UI 및 폰트 설정

--- TodaysWeather/TodaysWeather/AppDelegate.swift
+++ TodaysWeather/TodaysWeather/AppDelegate.swift
@@ -11,13 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        if #available(iOS 13, *) {
-//            let appearance = UINavigationBarAppearance()
-//            appearance.configureWithOpaqueBackground()
-//            appearance.backgroundColor = .clear
-//            UINavigationBar.appearance().standardAppearance = appearance
-//            UINavigationBar.appearance().scrollEdgeAppearance = appearance
-//        }
         return true
     }
 
--- TodaysWeather/TodaysWeather/Scene/DetailView/View/DetailViewController.swift
+++ TodaysWeather/TodaysWeather/Scene/DetailView/View/DetailViewController.swift
@@ -11,18 +11,26 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var discriptionLabel: UILabel!
+    @IBOutlet weak var currentTempTitleLabel: UILabel!
     @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var feelsTempTitleLabel: UILabel!
     @IBOutlet weak var feelsTemperatureLabel: UILabel!
+    @IBOutlet weak var minTempTitleLabel: UILabel!
     @IBOutlet weak var minTemperatureLabel: UILabel!
+    @IBOutlet weak var maxTempTitleLabel: UILabel!
     @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var humidityTitleLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windSpeedTitleLabel: UILabel!
     @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var pressureTitleLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
     var weatherInfo: WeatherInformation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureWeaterInfo()
+        self.configureStyle()
+        self.configureWeaterInfo()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "nightSky")!)
     }
     
@@ -43,6 +51,39 @@ class DetailViewController: UIViewController {
     }
     
     func configureStyle() {
+        self.navigationController?.navigationBar.tintColor = .white
+        self.cityNameLabel.textColor = .white
+        self.discriptionLabel.textColor = .white
+        self.currentTempTitleLabel.textColor = .white
+        self.currentTemperatureLabel.textColor = .white
+        self.feelsTempTitleLabel.textColor = .white
+        self.feelsTemperatureLabel.textColor = .white
+        self.minTempTitleLabel.textColor = .white
+        self.minTemperatureLabel.textColor = .white
+        self.maxTempTitleLabel.textColor = .white
+        self.maxTemperatureLabel.textColor = .white
+        self.humidityTitleLabel.textColor = .white
+        self.humidityLabel.textColor = .white
+        self.windSpeedTitleLabel.textColor = .white
+        self.windSpeedLabel.textColor = .white
+        self.pressureTitleLabel.textColor = .white
+        self.pressureLabel.textColor = .white
         
+        self.cityNameLabel.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        self.discriptionLabel.font = UIFont.systemFont(ofSize: 25)
+        self.currentTempTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.currentTemperatureLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.feelsTempTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.feelsTemperatureLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.minTempTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.minTemperatureLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.maxTempTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.maxTemperatureLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.humidityTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.humidityLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.windSpeedTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.windSpeedLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        self.pressureTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.pressureLabel.font = UIFont.systemFont(ofSize: 25, weight: .bold)
     }
 }
